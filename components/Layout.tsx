6fa5f357e2ab09b43bb66f711276eef5806fedda
노정은 <wjddms4107@naver.com>
2023-06-18 12:16:57 +0900

    fix : 폴더명 에러

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
-import Header from "./Header";
+// import Header from "./Header";
+import Header from "./header";
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
