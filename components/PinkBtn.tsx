df0cd37c055b3009b220597d01c2ceb21a12bccb
노정은 <wjddms4107@naver.com>
2023-06-21 23:04:25 +0900

    fix : vercel Link href 에러 수정

--- components/PinkBtn.tsx
+++ components/PinkBtn.tsx
@@ -8,7 +8,7 @@ interface IPinkBtnProps {
 export default function PinkBtn({ text, url }: IPinkBtnProps) {
   return (
     <span className="text-sm lg:text-base text-white p-2 ml-4 bg-pink rounded-[4px] cursor-pointer hover:bg-deepPink">
-      <Link href={url ? url : ""} legacyBehavior>
+      <Link href={url ?? ""} legacyBehavior>
         <a target="_blank" rel="noopener noreferrer">
           {text}
         </a>
